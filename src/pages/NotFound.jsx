e042e2441a133fa626f6f61010363bf47d93692e
nelioats <nelioats@gmail.com>
2024-05-08 23:22:22 -0300

    not

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,6 +6,7 @@ const NotFound = () => {
        <div class="row justify-content-center align-items-center">
             <div class="col-md-6 text-center">
               <img src="assets/img/notfound/6368620.jpg" alt="" className="img-fluid" />
+              <h1>ola</h1>
           </div>
         </div>
     </>
