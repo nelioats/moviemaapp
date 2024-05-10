a71f2fde3cf42191046f82148c9738f9ba975ba8
nelioats <nelioats@gmail.com>
2024-05-09 22:43:31 -0300

    notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,9 +5,8 @@ const NotFound = () => {
     <>
        <div class="row justify-content-center align-items-center">
             <div class="col-md-6 text-center">
-              <img src="assets/img/notfound/6368620.jpg" alt="" className="img-fluid" />
-              <img src="assets/img/notfound/6368620.jpg" alt="" className="img-fluid" />
-             
+              <img src="assets/img/notfound/6368620.jpg" alt="" className="img-fluid" />             
+              <img src="assets/img/notfound/6368620.jpg" alt="" className="img-fluid" />             
           </div>
         </div>
     </>
