6646ab1035139733ddb4ca4b8487a6dfdf7c3e66
nelioats <nelioats@gmail.com>
2024-05-17 17:37:13 -0300

    oi

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <Route path='/moviemaapp/projetos' element={<Projetos />} />
           <Route path='/moviemaapp/projeto/:project_slug' element={<ProjetoDescricao />} />
 
-          <Route path='/moviemaapp/*' element={<NotFound />} />
+          <Route path='*' element={<NotFound />} />
           <Route path='/moviemaapp/projeto/*' element={<NotFound />} />
         </Route>
 
